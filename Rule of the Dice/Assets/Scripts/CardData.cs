fd03e3f163741d75c5f4a0fb8ccdd3e0e3a3cafa
Damian Ochramowicz <damochramowicz260@gmail.com>
2022-07-16 15:07:35 +0200

    cards logic

---
+++ Rule of the Dice/Assets/Scripts/CardData.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [System.Serializable]
+    public struct CardData
+    {
+        public string Description;
+        public int HappinesValue;
+        public int PollutionValue;
+        public int MoneyValue;
+        public int DurationInTurns;
+    }
+}
--- Rule of the Dice/Assets/Scripts/GameController.cs
+++ Rule of the Dice/Assets/Scripts/GameController.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+﻿using Assets.Scripts;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
     public int TurnNumber;
+    public List<CardData> CardsSource;
     // Start is called before the first frame update
     void Start()
     {
