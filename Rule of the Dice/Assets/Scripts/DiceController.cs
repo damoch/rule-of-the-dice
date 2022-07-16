2bfdaf92a6464cd57d4cec83d804b7ba33e80ba3
Damian Ochramowicz <damochramowicz260@gmail.com>
2022-07-16 19:36:40 +0200

    dice roll mechanic

--- Rule of the Dice/Assets/Scripts/DiceController.cs
+++ Rule of the Dice/Assets/Scripts/DiceController.cs
@@ -10,26 +10,70 @@ public class DiceController : MonoBehaviour
     public Image DiceImage;
     private Dictionary<int, int> NumberToTextureId;
     public int Value;
+    public int NumberOfThrowsBeforeValueIsDetermined = 20;
+    [Range(0,1)]
+    public float FirstRollPauseSeconds = 0.1f;
+    [Range(0, 1)]
+    public float LastRollPauseSeconds = 1f;
+    public GameController GameController;
+
+    private float _currentPause;
+    private float _pauseIncrease;
+    private float _pauseElapsed;
+    private int _currentRoll = 0;
+
+    private bool _isRolling;
     void Start()
     {
+        GameController.RegisterDice(this);
+        _isRolling = false;
+
         NumberToTextureId = new Dictionary<int, int>();
         for(int i = 1; i < 7; i++)
         {
             NumberToTextureId.Add(i, i - 1);
         }
-        DiceRoll();
+        _pauseIncrease = (LastRollPauseSeconds - FirstRollPauseSeconds) / NumberOfThrowsBeforeValueIsDetermined;
+    }
+
+    public void BeginDiceRoll()
+    {
+        _isRolling = true;
+        _currentRoll = 0;
+        _currentPause = FirstRollPauseSeconds;
+    }
+
+    private void EndDiceRoll()
+    {
+        _isRolling = false;
+        Debug.Log($"Dice result: {Value}");
+        GameController.NotifyDicerollResult(Value, this);
     }
 
     private void DiceRoll()
     {
         Value = UnityEngine.Random.Range(1,6);
         DiceImage.sprite = Sprite.Create(Images[Value - 1], DiceImage.sprite.rect, DiceImage.sprite.pivot);
-        Debug.Log($"Dice result: {Value}");
+        _pauseElapsed = 0;
+        _currentPause += _pauseIncrease;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_isRolling)
+        {
+            _pauseElapsed += Time.deltaTime;
+            if(_pauseElapsed >= _currentPause)
+            {
+                _currentRoll++;
+                DiceRoll();
+                if (_currentRoll >= NumberOfThrowsBeforeValueIsDetermined)
+                {
+                    EndDiceRoll();
+                }
+            }
+
+        }
     }
 }
--- Rule of the Dice/Assets/Scripts/GameController.cs
+++ Rule of the Dice/Assets/Scripts/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using Assets.Scripts;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,6 +16,8 @@ public class GameController : MonoBehaviour
     public CurrentCardController CurrentCardController;
     public int MinimalNumberOfCardsInQueue;
     public Button NextTurnButton;
+    private Dictionary<DiceController, bool> _hasDiceEndedRoll;
+    public int CurrentDicerollValue;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,7 +35,17 @@ public class GameController : MonoBehaviour
         {
             SetCardGuiFor(item.Key, item.Value);
         }
-        NextTurn();
+        //NextTurn();
+    }
+
+    internal void RegisterDice(DiceController diceController)
+    {
+        if(_hasDiceEndedRoll == null)
+        {
+            _hasDiceEndedRoll = new Dictionary<DiceController, bool>();
+        }
+        Debug.Log("Registered dice " + diceController.gameObject.name);
+        _hasDiceEndedRoll.Add(diceController, true);
     }
 
     // Update is called once per frame
@@ -43,9 +56,50 @@ public class GameController : MonoBehaviour
 
     public void NextTurn()
     {
+        NextTurnButton.interactable = false;
         TurnNumber++;
         Debug.Log("Turn " + TurnNumber);
         TurnNumberText.text = $"Turn: {TurnNumber}";
+        StartDiceRoll();
+    }
+
+    internal void NotifyDicerollResult(int value, DiceController dc)
+    {
+        _hasDiceEndedRoll[dc] = true;
+
+        foreach (var dice in _hasDiceEndedRoll.Keys)
+        {
+            if (!_hasDiceEndedRoll[dice])
+            {
+                return;
+            }
+        }
+
+        EndDiceRoll();
+    }
+
+    private void EndDiceRoll()
+    {
+        var result = _hasDiceEndedRoll.Keys.Sum(x => x.Value);
+
+        Debug.Log($"Result = {result}");
+    }
+
+    private void StartDiceRoll()
+    {
+        //Ugly-ass hack :/
+        var d1 = _hasDiceEndedRoll.Keys.First();
+        var d2 = _hasDiceEndedRoll.Keys.Last();
+        _hasDiceEndedRoll[d1] = false;
+        _hasDiceEndedRoll[d2] = false;
+        d1.BeginDiceRoll();
+        d2.BeginDiceRoll();
+
+        //foreach (var dice in _hasDiceEndedRoll.Keys)
+        //{
+        //    dice.BeginDiceRoll();
+        //    _hasDiceEndedRoll[dice] = false;
+        //}
     }
 
     private CardData GetNewCard()
